4257f3bea0c1697335edaefff55b4089f6d09894
Kurbannn <kurbansadykov7@gmail.com>
2025-12-08 03:08:40 +0300

    UpdateHomework#4

--- main.js
+++ main.js
@@ -2,20 +2,16 @@
 const productCards = document.querySelectorAll('.card-container');
 const changeColorAllCardsButton = document.querySelector('#change-color-all-cards');
 const greenColorHash = '#00FF00';
-
 changeColorAllCardsButton.addEventListener('click', () => {
   productCards.forEach((card) => card.style.backgroundColor = greenColorHash);
 });
-
 // Покраска первой карточки
 const firstProductCard = document.querySelector('.card-container');
 const changeColorFirstCardButton = document.querySelector('#change-color-first-card');
 const lavenderColorHash = '#9669c9ff';
-
 changeColorFirstCardButton.addEventListener('click', () => {
   firstProductCard.style.backgroundColor = lavenderColorHash;
 });
-
 // Открыть Google
   const openGoogleButton = document.querySelector('#open-google');
   openGoogleButton.addEventListener('click',openGoogle);
@@ -29,23 +25,18 @@ changeColorFirstCardButton.addEventListener('click', () => {
  }
     //Вывод заголовка на консоль
     const mainTitle = document.querySelector('.choose-product-text');
-
     mainTitle.addEventListener('mouseenter', () => {
     console.log(mainTitle.textContent.trim()); })
-
     //переключение кнопок 
     const toggleColorBtn = document.querySelector('#toggle-color-btn');
     const color1 = '#9669c9ff'; 
     const color2 = '#00FF00'; 
-
     let isFirstColor = true;
     toggleColorBtn.addEventListener('click', function() {
      if (isFirstColor) {
-      
      this.style.backgroundColor = color2;
      this.style.color = '#000'; 
     } else {
-       
         this.style.backgroundColor = color1;
         this.style.color = '#fff'; 
     }
