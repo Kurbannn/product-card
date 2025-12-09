e2854a72a0813a4c0e059216ab666138db7998ee
Kurbannn <kurbansadykov7@gmail.com>
2025-12-10 02:34:10 +0300

    UpdateJS###4

--- main.js
+++ main.js
@@ -1,39 +1,31 @@
 // Покраска всех карточек
 const productCards = document.querySelectorAll('.card-container');
 const changeColorAllCardsButton = document.querySelector('#change-color-all-cards');
-const greenColorHash = '#00FF00';
+
 changeColorAllCardsButton.addEventListener('click', () => {
-  productCards.forEach((card) => card.style.backgroundColor = greenColorHash);
+    productCards.forEach((card) => {
+        card.classList.toggle('green-bg');
+    });
 });
 // Покраска первой карточки
 const firstProductCard = document.querySelector('.card-container');
 const changeColorFirstCardButton = document.querySelector('#change-color-first-card');
-const lavenderColorHash = '#9669c9ff';
 changeColorFirstCardButton.addEventListener('click', () => {
-  firstProductCard.style.backgroundColor = lavenderColorHash;
+    firstProductCard.classList.toggle('purple-bg');
 });
 // Открыть Google
-  const openGoogleButton = document.querySelector('#open-google');
-  openGoogleButton.addEventListener('click',openGoogle);
-  function  openGoogle() {
-  const answer = confirm ('Вы действительно хотите открыть Google');
-  if (answer === true) {
-      window.open('https://google.com');
- }else{
-        return
+const openGoogleButton = document.querySelector('#open-google');
+openGoogleButton.addEventListener('click', openGoogle);
+
+function openGoogle() {
+    const answer = confirm('Вы действительно хотите открыть Google');
+    if (answer === true) {
+        window.open('https://google.com');
     }
- }
-   const toggleColorBtn = document.querySelector('#toggle-color-btn');
-   const color1 = '#9669c9ff'; 
-   const color2 = '#00FF00';
-    toggleColorBtn.addEventListener('click', function() {
-    const isNowFirstColor = this.classList.toggle('first-color');
-    if (isNowFirstColor) {
-        this.style.backgroundColor = color1;
-        this.style.color = '#fff';
-    } else {
-        this.style.backgroundColor = color2;
-        this.style.color = '#000';
-    }
-    console.log(`Цвет кнопки изменен на: ${this.style.backgroundColor}`);
+}
+const toggleColorBtn = document.querySelector('#toggle-color-btn');
+toggleColorBtn.addEventListener('click', function() {
+    const isGreen = this.classList.toggle('color-green');
+    this.classList.toggle('color-purple', !isGreen);
+    console.log(`Цвет кнопки изменен на: ${isGreen ? 'зеленый' : 'фиолетовый'}`);
 });
