94c667fa815b6473b199153bee5a55a7172b88b0
Kurbannn <kurbansadykov7@gmail.com>
2026-01-23 03:56:13 +0300

    uphomework-6

--- homework-6.js
+++ homework-6.js
@@ -1,5 +1,4 @@
-
-
+// 3 задание 
 const person = {
     name: "Курбан",
     age: 30,
@@ -8,7 +7,6 @@ const person = {
     respublic: "Дагестан"
 };
 // 4 задание
-
 const car = {
     brand: "Киа",
     model: "Соренто",
@@ -16,41 +14,33 @@ const car = {
     transmission: "Автомат",
     color: "Черный",
 };
-car.owner = person
-console.log(`Владелец ${car.brand} ${car.model}: ${car.owner.name}`);
+  car.owner = person
+  console.log(`Владелец ${car.brand} ${car.model}: ${car.owner.name}`);
 
 // 5 задание
-
 function maxSpeed(carObject){
   if (carObject.maxSpeed){
     carObject.maxSpeed=190
   }
-
 };
-
-maxSpeed(car) 
+  maxSpeed(car);
 
 //6 задание
-
 function propertyValue(obj, propertyName) {
   console.log(obj[propertyName]);
 }
 
 //7 задание 
-
-const productlist = ['cheese' ,'milk', 'buhanka', 'banana']
+const productlist = ['cheese' ,'milk', 'buhanka', 'banana'];
 
 //8 задание 
-
 const bookList = [
   { title: 'Book 1', author: 'Author 1', year: 1979 },
   { title: 'Book 2', author: 'Author 2', year: 1988 },
   { title: 'Book 3', author: 'Author 3', year: 2002 },
   { title: 'Book 4', author: 'Author 4', year: 2005 },
 ];
-
 bookList.push({ title: 'Book 5', author: 'Author 5', year: 2012});
-
 console.log(bookList);
 
 // 9задание 
@@ -58,20 +48,16 @@ const mangaList=[
     {title:'One piece',  author:'Eiichiro Oda', year:'1997',
      title:'Berserk', author:'Kentaro Miura' , yaer:'1989',
      title:'Naruto', author:'Kentaro Miura' , yaer:'1989',
-     title:'Berserk', author:'Kentaro Miura' , yaer:'1989',
-        
+     title:'Berserk', author:'Kentaro Miura' , yaer:'1989',   
      }
-]
-
-    const unitedBookOfManga =[...bookList, ...mangaList];
-
-  console.log(unitedBookOfManga)
+];
+  const unitedBookOfManga =[...bookList, ...mangaList];
+  console.log(unitedBookOfManga);
 
   //10 задание 
-
   function newProperty(object) {
   object.isRare = object.year <= 2000;
   return object;
 }
-const updatedbookList = unitedBookOfManga.map( newProperty);
-console.log(updatedbookList);
+  const updatedbookList = unitedBookOfManga.map( newProperty);
+  console.log(updatedbookList);
