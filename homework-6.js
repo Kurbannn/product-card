5f91f37329c575cf9542395a3968870f6984bae1
Kurbannn <kurbansadykov7@gmail.com>
2026-01-28 15:52:13 +0300

    Homework#6

--- homework-6.js
+++ homework-6.js
@@ -17,12 +17,12 @@ const car = {
   car.owner = person
   console.log(`Владелец ${car.brand} ${car.model}: ${car.owner.name}`);
 // 5 задание
-    function maxSpeed(carObject){
-     if (carObject.maxSpeed){
-    carObject.maxSpeed=190
+    function getMaxSpeed(carObject){
+     if (carObject.getMaxSpeed){
+    carObject.getMaxSpeed=190
   }
 };
-  maxSpeed(car);
+  getMaxSpeed(car);
 //6 задание
    function propertyValue(obj, propertyName) {
     console.log(obj[propertyName]);
@@ -30,28 +30,28 @@ const car = {
 //7 задание 
 const productlist = ['cheese' ,'milk', 'buhanka', 'banana'];
 //8 задание 
-const bookList = [
+const findBookList = [
   { title: 'Book 1', author: 'Author 1', year: 1979 },
   { title: 'Book 2', author: 'Author 2', year: 1988 },
   { title: 'Book 3', author: 'Author 3', year: 2002 },
   { title: 'Book 4', author: 'Author 4', year: 2005 },
 ];
-bookList.push({ title: 'Book 5', author: 'Author 5', year: 2012});
-console.log(bookList);
+findBookList.push({ title: 'Book 5', author: 'Author 5', year: 2012});
+console.log(findBookList);
 // 9задание 
-   const mangaList =[
+   const keepMangaList =[
     {title:'One piece',  author:'Eiichiro Oda', year:'1997',
      title:'Berserk', author:'Kentaro Miura' , yaer:'1989',
      title:'Naruto', author:'Kentaro Miura' , yaer:'1989',
      title:'Berserk', author:'Kentaro Miura' , yaer:'1989',   
      }
 ];
-    const unitedBookOfManga =[...bookList, ...mangaList];
+    const unitedBookOfManga =[...findBookList, ...keepMangaList];
       console.log(unitedBookOfManga);
   //10 задание 
   function newProperty(object) {
     object.isRare = object.year <= 2000;
   return object;
 }
-  const updatedbookList = unitedBookOfManga.map( newProperty);
-    console.log(updatedbookList);
+  const updatedfindBookList = unitedBookOfManga.map( newProperty);
+    console.log(updatedfindBookList);
