467663f2d6680e39a75a5a58a8f80df7fff578ff
Kurbannn <kurbansadykov7@gmail.com>
2026-01-29 10:46:57 +0300

    uphomework№6

--- homework-6.js
+++ homework-6.js
@@ -17,12 +17,12 @@ const car = {
   car.owner = person
   console.log(`Владелец ${car.brand} ${car.model}: ${car.owner.name}`);
 // 5 задание
-    function getMaxSpeed(carObject){
-     if (carObject.getMaxSpeed){
-    carObject.getMaxSpeed=190
+    function setInfoMaxSpeed(carObject){
+     if (carObject.setInfoMaxSpeed){
+    carObject.setInfoMaxSpeed=190
   }
 };
-  getMaxSpeed(car);
+  setInfoMaxSpeed(car);
 //6 задание
    function propertyValue(obj, propertyName) {
     console.log(obj[propertyName]);
@@ -30,28 +30,28 @@ const car = {
 //7 задание 
 const productlist = ['cheese' ,'milk', 'buhanka', 'banana'];
 //8 задание 
-const findBookList = [
+const findBookListArray = [
   { title: 'Book 1', author: 'Author 1', year: 1979 },
   { title: 'Book 2', author: 'Author 2', year: 1988 },
   { title: 'Book 3', author: 'Author 3', year: 2002 },
   { title: 'Book 4', author: 'Author 4', year: 2005 },
 ];
-findBookList.push({ title: 'Book 5', author: 'Author 5', year: 2012});
-console.log(findBookList);
+    findBookListArray.push({ title: 'Book 5', author: 'Author 5', year: 2012});
+console.log(findBookListArray);
 // 9задание 
-   const keepMangaList =[
+   const getMangaListArray =[
     {title:'One piece',  author:'Eiichiro Oda', year:'1997',
      title:'Berserk', author:'Kentaro Miura' , yaer:'1989',
      title:'Naruto', author:'Kentaro Miura' , yaer:'1989',
      title:'Berserk', author:'Kentaro Miura' , yaer:'1989',   
      }
 ];
-    const unitedBookOfManga =[...findBookList, ...keepMangaList];
+    const unitedBookOfManga =[...findBookListArray, ...getMangaListArray];
       console.log(unitedBookOfManga);
   //10 задание 
-  function newProperty(object) {
+  function assignRarityFlag(object) {
     object.isRare = object.year <= 2000;
   return object;
 }
-  const updatedfindBookList = unitedBookOfManga.map( newProperty);
-    console.log(updatedfindBookList);
+  const updatedfindBookListArray = unitedBookOfManga.map( assignRarityFlag);
+  console.log(updatedfindBookListArray);
