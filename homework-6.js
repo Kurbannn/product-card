085a2e2eb84b066cbca02760ce167f0c9e625b91
Kurbannn <kurbansadykov7@gmail.com>
2026-01-31 11:07:38 +0300

    homework№6

--- homework-6.js
+++ homework-6.js
@@ -24,19 +24,33 @@ const car = {
 };
   setInfoMaxSpeed(car);
 //6 задание
-   function propertyValue(obj, propertyName) {
+   function addPropertyValue(obj, propertyName) {
     console.log(obj[propertyName]);
 }
 //7 задание 
 const productlist = ['cheese' ,'milk', 'buhanka', 'banana'];
 //8 задание 
 const findBookListArray = [
-  { title: 'Book 1', author: 'Author 1', year: 1979 },
-  { title: 'Book 2', author: 'Author 2', year: 1988 },
-  { title: 'Book 3', author: 'Author 3', year: 2002 },
-  { title: 'Book 4', author: 'Author 4', year: 2005 },
+  {  title: 'Book 1',
+     author: 'Author 1', 
+     year: 1979 },
+
+  {  title: 'Book 2',
+     author: 'Author 2',
+     year: 1988 },
+
+  {  title: 'Book 3',
+     author: 'Author 3',
+     year: 2002 },
+
+  {  title: 'Book 4',
+     author: 'Author 4',
+     year: 2005 },
 ];
-    findBookListArray.push({ title: 'Book 5', author: 'Author 5', year: 2012});
+    findBookListArray.push({
+      title: 'Book 5',
+      author: 'Author 5',
+      year: 2012});
 console.log(findBookListArray);
 // 9задание 
    const getMangaListArray =[
