0bf91001d9f769e94484410e00ae02ae4f9620aa
Kurbannn <kurbansadykov7@gmail.com>
2026-02-04 16:58:58 +0300

    homework№6

--- homework-6.js
+++ homework-6.js
@@ -1,71 +1,115 @@
-// 3 задание 
+// 3 задание — Создайте объект на основе ваших данных
 const person = {
-    name: "Курбан",
-    age: 30,
-    city: "Каспийск", 
-    from: "Россия",
-    respublic: "Дагестан"
+  name: "Курбан",
+  age: 30,
+  city: "Каспийск",
+  from: "Россия",
+  respublic: "Дагестан"
 };
-// 4 задание
+
+// 4 задание — Создайте объект, который будет хранить данные об автомобиле
 const car = {
-    brand: "Киа",
-    model: "Соренто",
-    year: 2017,
-    transmission: "Автомат",
-    color: "Черный",
+  brand: "Киа",
+  model: "Соренто",
+  year: 2017,
+  transmission: "Автомат",
+  color: "Черный"
 };
-  car.owner = person
-  console.log(`Владелец ${car.brand} ${car.model}: ${car.owner.name}`);
-// 5 задание
-    function setInfoMaxSpeed(carObject){
-     if (carObject.setInfoMaxSpeed){
-    carObject.setInfoMaxSpeed=190
+
+car.owner = person;
+console.log(`Владелец ${car.brand} ${car.model}: ${car.owner.name}`);
+
+// 5 задание — Написать функцию, которая аргументом будет принимать объект
+function setInfoMaxSpeed(carObject) {
+  if (!carObject.maxSpeed) {
+    carObject.maxSpeed = 190;
   }
-};
-  setInfoMaxSpeed(car);
-//6 задание
-   function addPropertyValue(obj, propertyName) {
-    console.log(obj[propertyName]);
 }
-//7 задание 
-const productlist = ['cheese' ,'milk', 'buhanka', 'banana'];
-//8 задание 
+
+setInfoMaxSpeed(car);
+
+// 6 задание — Написать функцию, которая получает первым аргументом объект, 
+// а вторым — имя свойства; выводит значение этого свойства в консоль
+function addPropertyValue(obj, propertyName) {
+  console.log(obj[propertyName]);
+}
+
+
+addPropertyValue(car, "maxSpeed"); 
+
+// 7 задание — Создать массив, который содержит названия продуктов
+const productList = [
+  "cheese",
+  "milk",
+  "buhanka",
+  "banana"
+];
+
+// 8 задание — Создать массив, состоящий из объектов, где объект представляет собой книгу
 const findBookListArray = [
-  {  title: 'Book 1',
-     author: 'Author 1', 
-     year: 1979 },
-
-  {  title: 'Book 2',
-     author: 'Author 2',
-     year: 1988 },
-
-  {  title: 'Book 3',
-     author: 'Author 3',
-     year: 2002 },
-
-  {  title: 'Book 4',
-     author: 'Author 4',
-     year: 2005 },
+  {
+    title: "Book 1",
+    author: "Author 1",
+    year: 1979
+  },
+  {
+    title: "Book 2",
+    author: "Author 2",
+    year: 1988
+  },
+  {
+    title: "Book 3",
+    author: "Author 3",
+    year: 2002
+  },
+  {
+    title: "Book 4",
+    author: "Author 4",
+    year: 2005
+  }
 ];
-    findBookListArray.push({
-      title: 'Book 5',
-      author: 'Author 5',
-      year: 2012});
+
+findBookListArray.push({
+  title: "Book 5",
+  author: "Author 5",
+  year: 2012
+});
+
 console.log(findBookListArray);
-// 9задание 
-   const getMangaListArray =[
-    {title:'One piece',  author:'Eiichiro Oda', year:'1997',
-     title:'Berserk', author:'Kentaro Miura' , yaer:'1989',
-     title:'Naruto', author:'Kentaro Miura' , yaer:'1989',
-     title:'Berserk', author:'Kentaro Miura' , yaer:'1989',   
-     }
+
+// 9 задание — Создать ещё один массив, состоящий из тех же книг (манга)
+const getMangaListArray = [
+  {
+    title: "One piece",
+    author: "Eiichiro Oda",
+    year: "1997"
+  },
+  {
+    title: "Berserk",
+    author: "Kentaro Miura",
+    year: "1989"
+  },
+  {
+    title: "Naruto",
+    author: "Masashi Kishimoto",
+    year: "1999"
+  },
+  {
+    title: "Berserk",
+    author: "Kentaro Miura",
+    year: "1989"
+  }
 ];
-    const unitedBookOfManga =[...findBookListArray, ...getMangaListArray];
-      console.log(unitedBookOfManga);
-  //10 задание 
-  function assignRarityFlag(object) {
-    object.isRare = object.year <= 2000;
+
+const unitedBookOfManga = [...findBookListArray, ...getMangaListArray];
+console.log(unitedBookOfManga);
+
+// 10 задание — Почитать про метод массива map
+
+function assignRarityFlag(object) {
+  object.isRare = object.year <= 2000;
   return object;
 }
-  const updatedfindBookListArray = unitedBookOfManga.map( assignRarityFlag);
-  console.log(updatedfindBookListArray);
+
+const updatedFindBookListArray = unitedBookOfManga.map(assignRarityFlag);
+console.log(updatedFindBookListArray);
